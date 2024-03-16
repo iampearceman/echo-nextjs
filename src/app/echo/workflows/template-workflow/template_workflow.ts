437b225ce75790788848ee68140ea2db339951c4
StarL0rdDude <63902456+StarL0rdDude@users.noreply.github.com>
2024-03-17 01:06:40 +0200

    fixes

--- src/app/echo/workflows/template-workflow/template_workflow.ts
+++ src/app/echo/workflows/template-workflow/template_workflow.ts
@@ -4,35 +4,61 @@ import { renderReactEmail } from '../template-workflow/react-email';
 export function setup_template_workflow(echo: Echo) {
     echo.workflow(
         "template-workflow",
-        async ({ step, payload }) => {
-            const email = await step.email(
-                "email-step",
-                async (inputs: { name: string; }) => {
-                    return {
-                        body: "its a body " + inputs.name + " Payload: " + payload.post_id,
-                        subject: "Test Subject ",
-                    };
-                },
-                {
-                    inputs: {
-                        type: "object",
-                        properties: {
-                            name: { type: "string" },
+        async ({ step }) => {
+            await step.email('send-email', async (inputs) => {
+                return {
+                    subject: "This is an email subject",
+                    body: renderReactEmail(inputs)
+                };
+            }, {
+                inputSchema: {
+                    type: "object",
+                    properties: {
+                        showJoinButton: { type: "boolean", default: true },
+                        username: { type: "string", default: "alanturing" },
+                        userImage: {
+                            type: "string",
+                            default:
+                                "https://react-email-demo-bdj5iju9r-resend.vercel.app/static/vercel-user.png",
+                            format: "uri",
+                        },
+                        invitedByUsername: { type: "string", default: "Alan" },
+                        invitedByEmail: {
+                            type: "string",
+                            default: "alan.turing@example.com",
+                            format: "email",
+                        },
+                        teamName: { type: "string", default: "Team Awesome" },
+                        teamImage: {
+                            type: "string",
+                            default:
+                                "https://react-email-demo-bdj5iju9r-resend.vercel.app/static/vercel-team.png",
+                            format: "uri",
+                        },
+                        inviteLink: {
+                            type: "string",
+                            default: "https://vercel.com/teams/invite/foo",
+                            format: "uri",
+                        },
+                        inviteFromIp: { type: "string", default: "204.13.186.218" },
+                        inviteFromLocation: {
+                            type: "string",
+                            default: "São Paulo, Brazil",
                         },
                     },
                 },
-            );
+            });
 
             await step.push(
                 "push-step",
-                async (inputs: { name: string; }) => {
+                async (inputs) => {
                     return {
                         body: "its a body " + inputs.name,
                         subject: "Test Subject ",
                     };
                 },
                 {
-                    inputs: {
+                    inputSchema: {
                         type: "object",
                         required: ["name"],
                         properties: {
@@ -44,13 +70,13 @@ export function setup_template_workflow(echo: Echo) {
 
             await step.sms(
                 "sms-step",
-                async (inputs: { name: string; }) => {
+                async (inputs) => {
                     return {
                         body: "its a body " + inputs.name,
                     };
                 },
                 {
-                    inputs: {
+                    inputSchema: {
                         type: "object",
                         required: ["name"],
                         properties: {
@@ -62,13 +88,13 @@ export function setup_template_workflow(echo: Echo) {
 
             await step.inApp(
                 "inapp-step",
-                async (inputs: { name: string; }) => {
+                async (inputs) => {
                     return {
                         body: "ayy" + inputs.name,
                     };
                 },
                 {
-                    inputs: {
+                    inputSchema: {
                         type: "object",
                         required: ["name"],
                         properties: {
@@ -80,13 +106,13 @@ export function setup_template_workflow(echo: Echo) {
 
             await step.chat(
                 "chat-step",
-                async (inputs: { name: string; }) => {
+                async (inputs) => {
                     return {
                         body: "its a body " + inputs.name,
                     };
                 },
                 {
-                    inputs: {
+                    inputSchema: {
                         type: "object",
                         required: ["name"],
                         properties: {
